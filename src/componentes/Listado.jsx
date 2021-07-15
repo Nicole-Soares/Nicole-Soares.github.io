7df65adca88b8625a23597decdd04a279c6d66fd
Nicole Soares <nicolesoares98@hotmail.com>
2021-07-14 23:26:29 -0300

    icono de loading

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Gif from "./Gif"
+import Hourglass from"../imagenes/Hourglass.gif"
 
 let apiKey = "2ZEPY7PWMdCbc4nWcgflY72LcVevldu4"
 let url = `https://api.giphy.com/v1/gifs/trending?api_key=2ZEPY7PWMdCbc4nWcgflY72LcVevldu4`;
@@ -8,7 +9,7 @@ let url = `https://api.giphy.com/v1/gifs/trending?api_key=2ZEPY7PWMdCbc4nWcgflY7
 export default function Listado({gifs}) {
   return (
     <div >
-      {gifs.length > 0 ? gifs.map((item)=>{ return<Gif imagen={item.images.original.url} />}):""}
+      {gifs.length > 0 ? gifs.map((item)=>{ return<Gif imagen={item.images.original.url} />}):<img src={Hourglass} alt="loading" />}
 
     </div>
   );
