232502525cc52389282219240e8cc95542a303bb
Nicole Soares <nicolesoares98@hotmail.com>
2021-07-22 17:51:45 -0300

    Modificaciones css

--- src/componentes/Gif.jsx
+++ src/componentes/Gif.jsx
@@ -1,15 +1,18 @@
-import React from "react"
+import React from "react";
 import "./Gif.css";
 
 export default function Gif(props) {
-
-  return(
-    <div className={`contenedor-gifs ${props.Dark ? "dark" : "light"}`}>
+  return (
+    <div className="contenedor-gifs">
       <a href={props.url} target="_blank" rel="noreferrer">
-    <img src={props.imagen} alt={props.titulo} width={props.ancho}></img>
-    </a>
+        <img
+          className={`gif ${props.Dark ? "dark" : "light"}`}
+          src={props.imagen}
+          alt={props.titulo}
+          width="350px"
+          height="300px"
+        ></img>
+      </a>
     </div>
-
-
-  )
+  );
 }
--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Gif from "./Gif"
 import Hourglass from"../imagenes/Hourglass.gif"
+import "./Listado.css"
 
 let apiKey = "2ZEPY7PWMdCbc4nWcgflY72LcVevldu4"
 let url = `https://api.giphy.com/v1/gifs/trending?api_key=2ZEPY7PWMdCbc4nWcgflY72LcVevldu4`;
@@ -14,7 +15,7 @@ export default function Listado({gifs, loading, isDark}) {
   }
 
   return (
-    <div >
+    <div className={`contenedor-listado ${isDark ? "dark" : "light"}`} >
       {loading ? <p>Loading...</p> : gifs.length > 0 ? gifs.map((item)=>{ return<Gif Dark={isDark} imagen={item.images.downsized.url} key={item.id} titulo={item.title} url={item.url} ancho={item.width} />}):<img src={Hourglass} alt="loading" />}
 
     </div>
